75834f86eaeb03c60a3f90faa629a7f0c4c6d188
Vladimir Orany <vladimir@orany.cz>
2017-09-22 14:18:31 +0200

    tests for missing properties report and type mapping

---
+++ dru/src/test/groovy/com/agorapulse/dru/MissingPropertiesReportSpec.groovy
@@ -0,0 +1,22 @@
+package com.agorapulse.dru
+
+import spock.lang.Specification
+
+/**
+ * Test for missing properties report.
+ */
+class MissingPropertiesReportSpec extends Specification {
+
+    void 'test empty report'() {
+        expect:
+            new MissingPropertiesReport().toString().contains('DRU SAYS: GOOD JOB, NOTHING IGNORED ')
+    }
+
+    void 'test non-empty report'() {
+        when:
+            MissingPropertiesReport report = new MissingPropertiesReport()
+            report.add(MissingProperty.create('foo', 'bar', [foo: 'bar'], Map))
+        then:
+            report.toString().contains('‖ Map      | bar      | foo      | [foo:bar] ‖')
+    }
+}
---
+++ dru/src/test/groovy/com/agorapulse/dru/TypeMappingSpec.groovy
@@ -0,0 +1,30 @@
+package com.agorapulse.dru
+
+import spock.lang.Specification
+
+class TypeMappingSpec extends Specification {
+
+    void 'test to string'() {
+        expect:
+            new TypeMapping<Map>('', Map).toString() == 'TypeMapping[Map]'
+    }
+
+    void 'test mapping'() {
+        when:
+            TypeMapping<PojoTester> mapping = new TypeMapping<PojoTester>('', PojoTester)
+            mapping.ignore {
+                numericalValue
+            }
+            mapping.ignore(['first', 'second'])
+            mapping.ignore('third', 'fourth')
+            mapping.when { foo == 'bar' }
+            mapping.and { zoo == 'lane' }
+            mapping.map(['numbers', 'number']) { to Object }
+        then:
+            mapping.map('foo').path == 'foo'
+            mapping.propertyMappings.size() == 3
+            mapping.conditions.size() == 2
+            mapping.ignored.size() == 5
+    }
+
+}
